22387f3ff8ac71ba6edb41e244679336cf9f1428
Silpa <silpa123@gmail.com>
2021-09-07 20:25:16 +0530

    file modified

--- self/practice1.py
+++ self/practice1.py
@@ -12,6 +12,50 @@
 #     for i in s:
 #         print(i**2)
 # squ(q)
-s="asdfg"
-for i in s:
-    s.index(i)
+# s="asdfg"
+# for i in s:
+#     s.index(i)
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>string formatting
+# input=incredible
+#output=in... in... incredible?
+
+
+# def stutter(word):
+# 		return '{0}... {0}... {1}?'.format(word[:2], word)
+# word=stutter("incredible")
+# print(word)
+#in... in... incredible?
+
+# def stutter(word):
+# 	return (2*(word[:2]+'... '))+word+'?'
+# word=stutter("incredible")
+# print(word)
+#in... in... incredible?
+
+# def stutter(word):
+# 	return word[0:2] + '... ' + word[0:2] + '... ' + word + '?'
+
+#stutter = lambda w: '{}... {}... {}?'.format(w[:2], w[:2], w)
+# s='{0}{1}{2}'.format("a","b","c")
+# print(s)
+
+def calculator(num1, op, num2):
+    if op=="+":
+             re=num1+num2
+             return re
+    elif op=="-":
+             re=num1-num2
+             return re
+    elif op=="*":
+             re=num1*num2
+             return re
+    else:
+         if num2==0:
+                 return "cant divide with zero"
+
+         else:
+                 re = num1 / num2
+                 return re
+
+res=calculator(10,"/",0)
+print(res)
--- stringmanipulation/intro.py
+++ stringmanipulation/intro.py
@@ -21,3 +21,8 @@
 # p=len(s)
 # print(p)
 
+def stutter(word):
+		return '{0}... {0}... {1}?'.format(word[:2], word)
+word=stutter("incredible")
+print(word)
+#in... in... incredible?
